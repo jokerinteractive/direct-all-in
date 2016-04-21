fe7414ef2c22e78066a43bbe356a2bbeb0a173a1
Anton Popov <a@jokerinteractive.ru>
2016-04-21 06:22:45 +0300

    close #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
 /* --------- plugins --------- */
 
 var
-	gulp        = require('gulp'),
-	jade        = require('gulp-jade'),
-	plumber     = require('gulp-plumber'),
-	less        = require('gulp-less');
+  gulp = require('gulp'),
+  jade = require('gulp-jade'),
+  plumber = require('gulp-plumber'),
+  less = require('gulp-less');
 
 /* --------- paths --------- */
 
 var
-	paths = {
-		jade : {
-			location: './jade/**/*.jade',
-			compiled: './jade/_pages/*.jade',
-			destination: '.'
-		},
+  paths = {
+    jade: {
+      location: './jade/**/*.jade',
+      compiled: './jade/_pages/*.jade',
+      destination: '.'
+    },
 
-		less : {
-			location    : './less/style.less',
-			watch		: './less/**/*.less',
-			entryPoint  : './css/style.css',
-			destination: './css'
-		}
-	};
+    less: {
+      location: './less/style.less',
+      watch: './less/**/*.less',
+      entryPoint: './css/style.css',
+      destination: './css'
+    }
+  };
 
 /* --------- jade --------- */
 
-gulp.task('jade', function() {
-	gulp.src(paths.jade.compiled)
-		.pipe(plumber())
-		.pipe(jade({
-			pretty: '\t',
-		}))
-		.pipe(gulp.dest(paths.jade.destination));
+gulp.task('jade', function () {
+  gulp.src(paths.jade.compiled)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: '\t'
+    }))
+    .pipe(gulp.dest(paths.jade.destination));
 });
 
 /* --------- less --------- */
 
 gulp.task('less', function () {
-	gulp.src(paths.less.location)
-		.pipe(plumber())
-    	.pipe(less())
-    	.pipe(gulp.dest(paths.less.destination));
+  gulp.src(paths.less.location)
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest(paths.less.destination));
 });
 
 /* --------- watch --------- */
 
-gulp.task('watch', function(){
-	gulp.watch(paths.jade.location, ['jade']);
-	gulp.watch(paths.less.watch, ['less']);
+gulp.task('watch', function () {
+  gulp.watch(paths.jade.location, ['jade']);
+  gulp.watch(paths.less.watch, ['less']);
 });
 
 /* --------- default --------- */
